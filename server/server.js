927b1bd8644345f30d21b15b64b575cb3791f299
ilker ALTIN <ilkerobi@gmail.com>
2017-09-08 21:44:27 +0300

    add PATCH /todos:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -64,6 +65,31 @@ app.delete('/todos/:id',(req,res) => {
         res.status(400).send();
     })
 });
+
+app.patch('/todos/:id', (req,res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body,['text','completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(404).send();
+    })
+})
 
 
 app.listen(port, () => {

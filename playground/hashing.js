1dbf70fdce00cbc5d9e9ef6f9a3dfec8ef8dd71e
ilker ALTIN <ilkerobi@gmail.com>
2017-09-10 11:44:52 +0300

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,24 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
+
+let password = '123abc!';
+
+/* bcrypt.genSalt(10, (err,salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    })
+}); */
+
+let hashedPassword = '$2a$10$/czoEerj2I02u/qHlJGqPukd9sHwFQvjtIo/8m6jyzGhzyK2tHDGG';
+
+bcrypt.compare(password, hashedPassword, (err,res) => {
+    console.log(res);
+})
+
+
+/* let data = {
     id: 10
 };
 
@@ -11,7 +28,7 @@ console.log(token);
 
 let decoded = jwt.verify(token, '123abc');
 
-console.log(decoded);
+console.log(decoded); */
 
 /* 
 let msg = 'this is third user';

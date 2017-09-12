df5c21165f0d39b943f36e067ca6f4725699bf9d
ilker ALTIN <ilkerobi@gmail.com>
2017-09-12 23:54:23 +0300

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -113,6 +113,17 @@ app.get('/users/me', authenticate, (req,res) => {
     res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email','password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
+})
+
 
 
 app.listen(port, () => {

be1f87984a8da00442f2a5a876800b02127ecf98
ilker ALTIN <ilkerobi@gmail.com>
2017-10-07 11:25:46 +0300

    add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,16 @@ UserSchema.methods.generateAuthToken = function() {
     })
 }
 
+UserSchema.methods.removeToken = function(token) {
+    let user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+}
+
 UserSchema.statics.findByToken = function(token) {
     let User = this;
     let decoded;

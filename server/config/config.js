70b261665e6071b92cbdf30eb43ddc94bee21b69
ilker ALTIN <ilkerobi@gmail.com>
2017-10-07 12:57:31 +0300

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,19 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
+if (env === 'development' || env === 'test') {
+    let config = require('./config.json');
+    let envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    })
+}
+
+
+/* if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if(env === 'test'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+} */
--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
     let user = this;
     let access = 'auth';
-    let token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+    let token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET).toString();
 
     user.tokens.push({ access, token });
 
@@ -67,7 +67,7 @@ UserSchema.statics.findByToken = function(token) {
     let decoded;
 
     try {
-        decoded = jwt.verify(token,'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (error) {
       /*   return new Promise((resolve,reject) => {
             reject();
